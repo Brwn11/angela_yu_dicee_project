3a50cfa938606173f38f3bdbc68dc6be26c11deb
Brwn11 <81251924+Brwn11@users.noreply.github.com>
2021-08-16 15:13:52 +0530

    with padding

--- lib/main.dart
+++ lib/main.dart
@@ -23,13 +23,19 @@ class DicePage extends StatelessWidget {
         child: Row(
           children: [
             Expanded(
-              child: Image.asset(
-                "images/dice1.png",
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset(
+                  "images/dice1.png",
+                ),
               ),
             ),
             Expanded(
-              child: Image.asset(
-                "images/dice1.png",
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset(
+                  "images/dice1.png",
+                ),
               ),
             ),
           ],
